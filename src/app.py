c74e10145809e4c4c237c7335fc4a9d9ea5b7217
humanova <craxes_humanova@hotmail.com>
2019-11-18 15:12:38 +0300

    main method

--- src/app.py
+++ src/app.py
@@ -23,3 +23,8 @@ def url():
         response = jsonify(dict(success=False))
 
     return response
+
+if __name__ == "__main__":
+    
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
